1e16e4270451f76c939b043f3e0814217f92bfe4
Die <dlibonati@leafnoise.io>
2025-01-04 16:15:30 -0300

    feat: better code and tests added

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 ## Getting Started
 
-1. Clone the repository on your computer using: `git clone URL`.
-2. Go to the APP directory.
-3. Install dependencies using `yarn install`.
-4. Use `yarn run dev` to run in develop this project
+1. Clone the repository
+2. Join to the correct path of the clone
+3. Execute: `yarn install` or `npm install`
+4. Execute: `yarn dev` or `npm dev`
 
 ## Description
 
@@ -19,7 +19,28 @@ In this page I replicated without gravity the famous mobile game "Stack Game", w
 
 ## Libraries used
 
-1. THREE JS
+#### Dependencies
+
+```
+"@types/jest": "^29.5.14"
+"@types/cannon": "^0.1.12"
+"@types/three": "^0.161.2"
+"cannon": "^0.6.2"
+"three": "^0.148.0"
+```
+
+#### devDependencies
+
+```
+"@testing-library/dom": "^10.4.0"
+"@testing-library/jest-dom": "^6.6.3"
+"@testing-library/user-event": "^14.5.2"
+"jest": "^29.7.0"
+"jest-environment-jsdom": "^29.7.0"
+"ts-jest": "^29.2.5"
+"typescript": "^5.3.3"
+"vite": "^4.0.4"
+```
 
 ## Portfolio Link
 
@@ -33,4 +54,7 @@ https://github.com/DiegoLibonati/Stack-Game-3D-Page/assets/99032604/67e0b504-95d
 
 [`https://stack-game-3-d-page.vercel.app/`](https://stack-game-3-d-page.vercel.app/)
 
+## Testing
 
+1. Join to the correct path of the clone
+2. Execute: `yarn test` or `npm test`
---
+++ src/tests/jest.setup.ts
@@ -0,0 +1,66 @@
+import "@testing-library/jest-dom";
+
+import fs from "fs";
+import path from "path";
+
+const INITIAL_HTML: string = fs.readFileSync(
+  path.resolve(__dirname, "../../index.html"),
+  "utf8"
+);
+
+export const OFFICIAL_BODY = INITIAL_HTML.match(
+  /<body[^>]*>([\s\S]*?)<\/body>/i
+)![1];
+
+// Mocks
+
+jest.mock("three", () => {
+  const originalThree = jest.requireActual("three");
+
+  return {
+    ...originalThree,
+    Scene: jest.fn(() => ({
+      add: jest.fn(),
+      clear: jest.fn(),
+      remove: jest.fn(),
+    })),
+    WebGLRenderer: jest.fn(() => ({
+      setSize: jest.fn(),
+      setPixelRatio: jest.fn(),
+      render: jest.fn(),
+      setAnimationLoop: jest.fn(),
+    })),
+    OrthographicCamera: jest.fn(() => ({
+      lookAt: jest.fn(),
+      position: {
+        set: jest.fn(),
+      },
+      updateProjectionMatrix: jest.fn(),
+    })),
+    AmbientLight: jest.fn(),
+    DirectionalLight: jest.fn(() => ({
+      position: {
+        set: jest.fn(),
+      },
+    })),
+  };
+});
+
+jest.mock("cannon", () => {
+  const originalCannon = jest.requireActual("cannon");
+
+  return {
+    ...originalCannon,
+    World: jest.fn(() => ({
+      addBody: jest.fn(),
+      step: jest.fn(),
+      gravity: {
+        set: jest.fn(),
+      },
+      solver: {
+        iterations: 40,
+      },
+    })),
+    NaiveBroadphase: jest.fn(),
+  };
+});
